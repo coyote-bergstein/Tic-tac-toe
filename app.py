cf5a74f72291f829ee872efc5ebb26229acf035e
coyote-bergstein <dddddd0234@gmail.com>
2020-05-25 16:48:07 +0200

    Improve code readability simplify structure, add comments.

--- app.py
+++ app.py
@@ -1,95 +1,84 @@
-def input_command(command):
-    matrix = [["0", "0", "0"],
-              ["0", "0", "0"],
-              ["0", "0", "0"]]
-    for row in range(3):
-        for column in range(3):
-            matrix[row][column] = command[row * 3 + column]
-    return matrix
+#!/usr/bin/env python3
+class Game:
+    def __init__(self):
+        self.matrix = [["_", "_", "_"],
+                       ["_", "_", "_"],
+                       ["_", "_", "_"]]
+        self.column = 0
+        self.row = 0
+        self.character_turn = "X"
+        self.winner = None      # It's take 4 values None, X, O, draw.
 
-
-def input_cord(matrix, sign, column=-1, row=-1):
-    try:
-        column = int(column)
-        row = int(row)
-    except ValueError:
-        return "You should enter numbers!"
-    if column < 0 or column > 2 or row < 0 or row > 2:
-        return "Coordinates should be from 1 to 3!"
-    if matrix[row][column] != "_":
-        return "This cell is occupied! Choose another one!"
-    else:
-        matrix[row][column] = sign
-        return ""
-
-
-def status_check(matrix):
-    x_amount = 0
-    o_amount = 0
-    for row in range(3):
-        for column in range(3):
-            if matrix[row][column] == "X":
-                x_amount += 1
-            if matrix[row][column] == "O":
-                o_amount += 1
-
-    flag = []
-    for column in range(3):
-        if matrix[0][column] == matrix[1][column] == matrix[2][column]:
-            if matrix[0][column] != "_":
-                flag.append(matrix[0][column])
-    for row in range(3):
-        if matrix[row][0] == matrix[row][1] == matrix[row][2]:
-            if matrix[row][0] != "_":
-                flag.append(matrix[row][0])
-    if matrix[0][0] == matrix[1][1] == matrix[2][2]:
-        if matrix[0][0] != "_":
-            flag.append(matrix[0][0])
-    if matrix[0][2] == matrix[1][1] == matrix[2][0]:
-        if matrix[0][2] != "_":
-            flag.append(matrix[0][2])
-    if ("X" in flag and "O" in flag) or abs(x_amount - o_amount) >= 2:
-        return "Impossible"
-    elif len(flag) == 0:
-        if "_" in matrix[0] or "_" in matrix[1] or "_" in matrix[2]:
-            return "Game not finished"
+    def input_coordinates(self, row="-1", column="-1"):
+        if not row.isdigit() or not column.isdigit():
+            print("You should enter numbers!")
         else:
-            return "Draw"
-    else:
-        return f"{flag[0]} wins"
+            column = int(column) - 1
+            row = int(row) - 1
+            if column < 0 or column > 2 or row < 0 or row > 2:
+                print("Coordinates should be from 1 to 3!")
+            elif self.matrix[row][column] != "_":
+                print("This cell is occupied! Choose another one!")
+            else:
+                self.column = column
+                self.row = row
+                self.insert_sign()
 
+    def insert_sign(self):
+        self.matrix[self.row][self.column] = self.character_turn
+        self.change_turn()
+        self.check_game_status()
 
-def draw(matrix):
-    battlefield = "---------\n"
-    for row in range(3):
-        for column in range(3):
-            if column == 0:
-                battlefield += "| "
-            battlefield += f"{matrix[row][column]} "
-            if column == 2:
-                battlefield += "|\n"
-    battlefield += "---------"
-    print(battlefield)
-#     print(f"""---------
-# | {matrix[0][0]} {matrix[0][1]} {matrix[0][2]} |
-# | {matrix[1][0]} {matrix[1][1]} {matrix[1][2]} |
-# | {matrix[2][0]} {matrix[2][1]} {matrix[2][2]} |
-# ---------""")
+    def check_game_status(self):
+        # This part checks arrangement vertical and horizontal
+        for coordinate in range(3):
+            if self.matrix[0][coordinate] == self.matrix[1][coordinate] == self.matrix[2][coordinate]:
+                if self.matrix[0][coordinate] != "_":
+                    self.winner = self.matrix[0][coordinate]
+            if self.matrix[coordinate].count("X") == 3 or self.matrix[coordinate].count("O") == 3:
+                if self.matrix[coordinate][0] != "_":
+                    self.winner = self.matrix[coordinate][0]
 
+        # This check cross arrangement
+        if self.matrix[0][0] == self.matrix[1][1] == self.matrix[2][2]:
+            if self.matrix[0][0] != "_":
+                self.winner = self.matrix[0][0]
+        if self.matrix[0][2] == self.matrix[1][1] == self.matrix[2][0]:
+            if self.matrix[0][2] != "_":
+                self.winner = self.matrix[0][2]
 
-state = input_command("_________")
-draw(state)
-message = ""
-sign = "X"
-while message != "Draw" and message != "X wins" and message != "O wins":
-    status = input_cord(state, sign, *input("Enter the coordinates (column row): ").split())
-    if status != "":
-        print(status)
-    else:
-        draw(state)
-        if sign == "O":
-            sign = "X"
+        if "_" not in self.matrix[0] and "_" not in self.matrix[1] and "_" not in self.matrix[2]:
+            self.winner = "draw"
+
+    def change_turn(self):
+        if self.character_turn == "X":
+            self.character_turn = "O"
         else:
-            sign = "O"
-    message = status_check(state)
-print(message)
+            self.character_turn = "X"
+
+    def draw(self):
+        battlefield = "---------\n"
+        for row in range(3):
+            for column in range(3):
+                if column == 0:
+                    battlefield += "| "
+                battlefield += f"{self.matrix[row][column]} "
+                if column == 2:
+                    battlefield += "|\n"
+        battlefield += "---------"
+        print(battlefield)
+    
+    def play(self):
+        while self.winner is None:
+            self.draw()
+            self.input_coordinates(*input().split(maxsplit=1))
+            self.check_game_status()
+        self.draw()
+        if self.winner == "draw":
+            print(self.winner)
+        else:
+            print(self.winner, "wins")
+
+
+tic_tac_toe = Game()
+tic_tac_toe.play()
